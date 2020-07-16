331968acc4c8e9d5b605bc314860b3c1d1c3a3f4
Jeziel Marques <jezielmarques7@gmail.com>
2020-07-15 22:51:10 -0300

    RN Bank - initial ci

---
+++ src/pages/index.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+import { View, Text, Switch, TextInput } from 'react-native';
+
+import Slider from '@react-native-community/slider';
+import {Picker} from '@react-native-community/picker';
+
+import styles from './styles';
+
+const pages = () => {
+  return (
+    <View style={styles.container}>
+      
+    </View>
+  );
+}
+
+export default pages;
